321df0166d939c578cb2919da3064cd4bb9ea7fd
junikang96 <junikang96@gmail.com>
2025-05-06 03:11:41 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.c
+++ 백준/Bronze/15829. Hashing/Hashing.c
@@ -1,3 +1,4 @@
+#if 0
 #include <stdio.h>
 #include <math.h>
 
@@ -20,13 +21,53 @@ int main(void)
     char str[L];
     scanf("%s", str);
 
-    int hash = 0;
+    long long hash = 0;
     for (int i = 0; i < L; i++)
     {
         int element = change_to_integer(str[i]);
         hash += element * pow(31, i);
     }
 
-    printf("%d\n", hash);
+    printf("%lld\n", hash);
     return 0;
 }
+#endif
+
+#if 1
+#include <stdio.h>
+
+int MOD = 1234567891;
+
+int change_to_integer(char c)
+{
+    if (c >= 'a' && c <= 'z')
+    {
+        return c - 'a' + 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main(void)
+{
+    int L;
+    scanf("%d", &L);
+    char str[L];
+    scanf("%s", str);
+
+    long long hash = 0;
+    long long r = 1;
+
+    for (int i = 0; i < L; i++)
+    {
+        int element = change_to_integer(str[i]);
+        hash = (hash + element * r) % MOD;
+        r = (r * 31) % MOD;
+    }
+
+    printf("%lld\n", hash);
+    return 0;
+}
+#endif
